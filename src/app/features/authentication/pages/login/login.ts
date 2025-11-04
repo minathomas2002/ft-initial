e645dd4d4afe130c3f8caabe0c2f925d53f7d315
Mina Thomas <mina.thomas@jathwa.com>
2025-11-04 22:49:28 +0200

    (Authentication) implement UI for login page

--- src/app/features/authentication/pages/login/login.ts
+++ src/app/features/authentication/pages/login/login.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CheckboxModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
+      // call your login API here
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
