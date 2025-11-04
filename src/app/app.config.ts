e3b7958401a94109df0363045b077a591c989b6b
mahmoudsalama153 <mahmoud.salama@flairstech.com>
2025-11-04 13:39:42 +0200

    add app configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,35 @@
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingOptions, provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
-
+import { PRIMENG_CONFIG } from './core/configs/primeng-config';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error/error.interceptor';
+import { MessageService } from 'primeng/api';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+const scrollConfig: InMemoryScrollingOptions = {
+	scrollPositionRestoration: "disabled",
+};
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    		provideRouter(
+			routes,
+			withInMemoryScrolling(scrollConfig),
+			withComponentInputBinding(),
+		),
+    ...PRIMENG_CONFIG,
+    provideHttpClient(
+			withFetch(),
+			withInterceptors([authInterceptor, errorInterceptor]),
+		),
+    MessageService,
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: "dd MMM yyyy" },
+    },
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardView } from "./features/dashboard/pages/dashboard-view/dashboard-view";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DashboardView],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
