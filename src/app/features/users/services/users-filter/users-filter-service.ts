b75e3b6d58f6cbd241cc8bdc250f25a40299dda8
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-11-09 10:41:11 +0200

    Refactor UsersFilterService for improved readability and consistency
    - Reformatted code for better alignment and readability.
    - Ensured consistent indentation and spacing throughout the service methods.
    - No functional changes were made, focusing solely on code style improvements.

--- src/app/features/users/services/users-filter/users-filter-service.ts
+++ src/app/features/users/services/users-filter/users-filter-service.ts
@@ -7,43 +7,43 @@ import { UsersFilter } from '../../classes/users-filter';
 import { UsersStore } from 'src/app/shared/stores/users/users.store';
 
 @Injectable()
-export class UsersFilterService extends AbstractServiceFilter<IUsersFilter>{
-  	store = inject(UsersStore);
-	filterClass = new UsersFilter();
-	filter = signal(this.filterClass.filter);
+export class UsersFilterService extends AbstractServiceFilter<IUsersFilter> {
+  store = inject(UsersStore);
+  filterClass = new UsersFilter();
+  filter = signal(this.filterClass.filter);
 
-	showClearAll = computed(() => {
-		return false;
-	});
+  showClearAll = computed(() => {
+    return false;
+  });
 
-	performFilter$() {
-		this.resetPagination();
-		const filtration = new UsersFilterInterfaceAdapter(this.filter());
-		return this.store.getUsers(filtration.adaptFilter());
-	}
+  performFilter$() {
+    this.resetPagination();
+    const filtration = new UsersFilterInterfaceAdapter(this.filter());
+    return this.store.getUsers(filtration.adaptFilter());
+  }
 
-	clearAllFilters() {
-		this.clearAll();
-		this.applyFilter();
-	}
+  clearAllFilters() {
+    this.clearAll();
+    this.applyFilter();
+  }
 
-	get FilterRequest(): IUsersFilterRequest {
-		const filtration = new UsersFilterInterfaceAdapter(this.filter());
-		return filtration.adaptFilter();
-	}
+  get FilterRequest(): IUsersFilterRequest {
+    const filtration = new UsersFilterInterfaceAdapter(this.filter());
+    return filtration.adaptFilter();
+  }
 
-	resetOptionalFilters() {
-		this.filter.set({
-			...this.filter(),
-		});
-	}
+  resetOptionalFilters() {
+    this.filter.set({
+      ...this.filter(),
+    });
+  }
 
-	applyFilterWithPaging() {
-		this.updateFilterSignal();
-		const filtration = new UsersFilterInterfaceAdapter(this.filter());
-		this.store
-			.getUsers(filtration.adaptFilter())
-			.pipe(take(1))
-			.subscribe();
-	}
+  applyFilterWithPaging() {
+    this.updateFilterSignal();
+    const filtration = new UsersFilterInterfaceAdapter(this.filter());
+    this.store
+      .getUsers(filtration.adaptFilter())
+      .pipe(take(1))
+      .subscribe();
+  }
 }
