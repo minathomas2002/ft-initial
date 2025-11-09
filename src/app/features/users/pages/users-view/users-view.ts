8f75f7621ced62f66d0162eef2591b7a6724cdbd
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-11-09 11:17:18 +0200

    Add user role change functionality in UsersView component
    - Integrated ChangeRoleDialog into the UsersView template for managing user roles.
    - Implemented logic to handle role changes, including user selection and confirmation.
    - Utilized ChangeRoleFormService for role management and ToasterService for success notifications.
    - Enhanced user experience by allowing role changes directly from the users view.

--- src/app/features/users/pages/users-view/users-view.ts
+++ src/app/features/users/pages/users-view/users-view.ts
@@ -1,19 +1,22 @@
+import { ChangeRoleFormService } from './../../services/change-role-form/change-role-form-service';
 import { TooltipModule } from 'primeng/tooltip';
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
-import { take } from 'rxjs';
+import { finalize, take } from 'rxjs';
 import { TableLayoutComponent } from 'src/app/shared/components/layout-components/table-layout/table-layout.component';
 import { TableSkeletonComponent } from 'src/app/shared/components/skeletons/table-skeleton/table-skeleton.component';
 import { UsersLookupsStore } from 'src/app/shared/stores/users/users-lookups.store';
 import { UsersFilter } from '../../components/users-filter/users-filter';
-import { ITableHeaderItem, IUser, IUserRecord, TUsersSortingKeys } from 'src/app/shared/interfaces';
+import { ISelectItem, ITableHeaderItem, IUser, IUserRecord, TUsersSortingKeys } from 'src/app/shared/interfaces';
 import { UsersStore } from 'src/app/shared/stores/users/users.store';
 import { DataTableComponent } from 'src/app/shared/components/layout-components/data-table/data-table.component';
 import { UsersFilterService } from '../../services/users-filter/users-filter-service';
-import { EUserStatus } from 'src/app/shared/enums/users.enum';
+import { EUserRole, EUserStatus } from 'src/app/shared/enums/users.enum';
 import { UserStatusMapper } from '../../classes/user-status-mapper';
 import { BaseTagComponent } from 'src/app/shared/components/base-components/base-tag/base-tag.component';
 import { UsersActionMenu } from '../../components/users-action-menu/users-action-menu';
 import { DatePipe } from '@angular/common';
+import { ChangeRoleDialog } from '../../components/change-role-dialog/change-role-dialog';
+import { ToasterService } from 'src/app/shared/services/toaster/toaster.service';
 
 @Component({
   selector: 'app-users-view',
@@ -21,8 +24,12 @@ import { DatePipe } from '@angular/common';
     TableLayoutComponent,
     TableSkeletonComponent,
     DatePipe,
-    UsersFilter, BaseTagComponent, DataTableComponent, TooltipModule,
+    UsersFilter,
+    BaseTagComponent,
+    DataTableComponent,
+    TooltipModule,
     UsersActionMenu,
+    ChangeRoleDialog,
     DatePipe
   ],
   templateUrl: './users-view.html',
@@ -74,7 +81,10 @@ export class UsersView implements OnInit {
   totalRecords = computed(() => this.usersStore.count());
   userStatusMapper = new UserStatusMapper();
   selectedRows = signal<IUserRecord[]>([]);
-
+  changeRoleDialogVisible = signal<boolean>(false);
+  selectedUser = signal<IUser | null>(null);
+  changeRoleFormService = inject(ChangeRoleFormService);
+  ToasterService = inject(ToasterService);
   ngOnInit(): void {
     this.usersLookupsStore.getUserTitles()
       .pipe(take(1))
@@ -85,9 +95,26 @@ export class UsersView implements OnInit {
     return this.userStatusMapper.mapUserStatusColor()[status];
   }
   onChangeRole(item: IUser) {
-    console.log(item);
+    this.selectedUser.set(item);
+    this.changeRoleDialogVisible.set(true);
   }
   onDelete(item: IUser) {
     console.log(item);
+  }
+  onChangeRoleConfirm() {
+    const role = this.changeRoleFormService.role.value as unknown as ISelectItem;
+    this.changeRoleDialogVisible.set(false);
+    this.selectedUser.set(null);
+    this.usersStore.changeUserRole(this.selectedUser()?.id!, role.id as unknown as EUserRole)
+      .pipe(finalize(() => {
+        this.changeRoleDialogVisible.set(false);
+        this.selectedUser.set(null);
+      }))
+      .subscribe({
+        next: () => {
+          this.ToasterService.success('User role changed successfully');
+          this.filterService.applyFilter();
+        },
+      });
   }
 }
