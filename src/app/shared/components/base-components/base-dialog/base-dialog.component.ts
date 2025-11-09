769fca67e551daddb4a378bb537eb4a53e3c387e
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-11-09 10:40:46 +0200

    Enhance base dialog component and integrate dialog functionality
    - Added DIALOG_COMPONENT to PrimeNG configuration.
    - Updated base-dialog component HTML for improved styling and accessibility.
    - Renamed title input to dialogTitle for clarity and consistency.

--- src/app/shared/components/base-components/base-dialog/base-dialog.component.ts
+++ src/app/shared/components/base-components/base-dialog/base-dialog.component.ts
@@ -3,33 +3,31 @@ import { ButtonModule } from "primeng/button";
 import { type Dialog, DialogModule } from "primeng/dialog";
 import { DividerModule } from "primeng/divider";
 @Component({
-	selector: "app-base-dialog",
-	imports: [DialogModule, ButtonModule, DividerModule],
-	templateUrl: "./base-dialog.component.html",
-	styleUrl: "./base-dialog.component.scss",
+  selector: "app-base-dialog",
+  imports: [DialogModule, ButtonModule, DividerModule],
+  templateUrl: "./base-dialog.component.html",
+  styleUrl: "./base-dialog.component.scss",
 })
 export class BaseDialogComponent {
-	visible = model<boolean>(false);
-	onConfirm = output();
-	onShow = output();
-	onClose = output();
-	icon = input<string>("icon-eye");
-	title = input<string>("", {
-		alias: "dialog-title",
-	});
-	confirmLabel = input<string>("Submit");
-	showCloseButton = input<boolean>(true);
-	showConfirmButton = input<boolean>(true);
-	isLoading = input<boolean>(false);
-	contentStyleClass = input<string>("");
-	styleClass = input<string>("");
-	dialogRef = viewChild.required<Dialog>("dialogRef");
-	isConfirmationDisabled = input(false);
-	confirmButtonClass = input("w-[50%]");
-	cancelButtonClass = input("w-[50%]");
-	appendTo = input("body");
+  visible = model<boolean>(false);
+  onConfirm = output();
+  onShow = output();
+  onClose = output();
+  icon = input<string>("icon-eye");
+  dialogTitle = input<string>("");
+  confirmLabel = input<string>("Submit");
+  showCloseButton = input<boolean>(true);
+  showConfirmButton = input<boolean>(true);
+  isLoading = input<boolean>(false);
+  contentStyleClass = input<string>("");
+  styleClass = input<string>("");
+  dialogRef = viewChild.required<Dialog>("dialogRef");
+  isConfirmationDisabled = input(false);
+  confirmButtonClass = input("w-[50%]");
+  cancelButtonClass = input("w-[50%]");
+  appendTo = input("body");
 
-	closeCallback(e: Event): void {
-		this.dialogRef()?.close(e);
-	}
+  closeCallback(e: Event): void {
+    this.dialogRef()?.close(e);
+  }
 }
