941982365e933c20514175a64f91d19b40e68d7a
Mina Thomas <mina.thomas@flairstech.com>
2025-11-16 18:36:30 +0200

    (opportunities) append some opportunities filter

--- src/app/features/opportunities/components/opportunities-filter/opportunities-filters.ts
+++ src/app/features/opportunities/components/opportunities-filter/opportunities-filters.ts
@@ -7,23 +7,41 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 import { catchError, debounceTime, distinctUntilChanged, of, Subject, switchMap } from 'rxjs';
 import { OpportunitiesFilterService } from 'src/app/features/opportunities/services/opportunities-filter/opportunities-filter-service';
+import { EOpportunityStatus, EOpportunityType } from 'src/app/shared/enums/opportunities.enum';
+import { TranslatePipe } from 'src/app/shared/pipes';
 
 @Component({
   selector: 'app-opportunities-filters',
-  imports: [IconFieldModule, InputIconModule, FormsModule, InputTextModule, SelectModule],
+  imports: [
+    IconFieldModule,
+    InputIconModule,
+    FormsModule,
+    InputTextModule,
+    SelectModule,
+    TranslatePipe,
+  ],
   templateUrl: './opportunities-filters.html',
   styleUrl: './opportunities-filters.scss',
 })
 export class OpportunitiesFilters implements OnInit {
-  
   filterService = inject(OpportunitiesFilterService);
   destroyRef = inject(DestroyRef);
-  
-  isAdminMode = input.required<boolean>()
-  
+
+  isAdminMode = input.required<boolean>();
+
   filter = this.filterService.filter;
   searchSubject = new Subject<string>();
-  
+
+  opportunityStatuses = [
+    { label: 'opportunity.status.published', value: EOpportunityStatus.DRAFT },
+    { label: 'opportunity.status.draft', value: EOpportunityStatus.PUBLISHED },
+  ];
+
+  opportunityTypes = [
+    { label: 'opportunity.type.material', value: EOpportunityType.MATERIAL },
+    { label: 'opportunity.type.service', value: EOpportunityType.SERVICES },
+  ];
+
   ngOnInit() {
     this.listenToSearchText();
   }
@@ -34,7 +52,7 @@ export class OpportunitiesFilters implements OnInit {
         debounceTime(700),
         distinctUntilChanged(),
         switchMap((res) => this.performFilter$()),
-        takeUntilDestroyed(this.destroyRef),
+        takeUntilDestroyed(this.destroyRef)
       )
       .subscribe();
   }
--- src/app/shared/enums/opportunities.enum.ts
+++ src/app/shared/enums/opportunities.enum.ts
@@ -2,3 +2,8 @@ export enum EOpportunityStatus {
   PUBLISHED = 1,
   DRAFT = 2,
 }
+
+export enum EOpportunityType {
+  SERVICES = 1,
+  MATERIAL = 2,
+}
