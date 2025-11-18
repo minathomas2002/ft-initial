0950cf9a59aa552c024bde25aa71582fb5aba049
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-11-18 11:31:54 +0200

    error interceptor

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -2,17 +2,18 @@ import { type HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { inject } from '@angular/core';
 import { ToasterService } from '../../../shared/services/toaster/toaster.service';
+import { IBaseApiResponse } from 'src/app/shared/interfaces/api.interface';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-	const toastr = inject(ToasterService);
-	return next(req).pipe(
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		tap((response: any) => {
-			if (response instanceof HttpResponse && response.body?.hasError) {
-				toastr.error(
-					response.body.errorMessage || 'failed to process ,check your internet connection',
-				);
-			}
-		}),
-	);
+  const toaster = inject(ToasterService);
+  return next(req).pipe(
+    tap((response: any) => {
+      const responseBody = response as IBaseApiResponse<unknown>;
+      if (response instanceof HttpResponse && !responseBody.success) {
+        toaster.error(
+          responseBody.message || 'failed to process ,check your internet connection',
+        );
+      }
+    }),
+  );
 };
