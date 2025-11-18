e0f28210573fdf8106692993ba69008456ab64a4
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-11-18 12:45:40 +0200

    feat:opportunities api service

--- src/app/shared/api/opportunities/opportunities-api-service.ts
+++ src/app/shared/api/opportunities/opportunities-api-service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { BaseHttpService } from '../../services/Base-HTTP/base-Http.service';
 import { IOpportunitiesFilterRequest, IOpportunityRecord } from '../../interfaces/opportunities.interface';
 import { Observable } from 'rxjs';
-import { IApiPaginatedResponse } from '../../interfaces';
+import { IApiPaginatedResponse, IBaseApiResponse } from '../../interfaces';
 import { API_ENDPOINTS } from '../api-endpoints';
 
 @Injectable({
@@ -11,11 +11,11 @@ import { API_ENDPOINTS } from '../api-endpoints';
 export class OpportunitiesApiService {
   private readonly baseHttpService = inject(BaseHttpService);
 
-  getOpportunities(filter: IOpportunitiesFilterRequest): Observable<IApiPaginatedResponse<IOpportunityRecord[]>> {
-    return this.baseHttpService.get<IOpportunityRecord[], unknown>(API_ENDPOINTS.opportunities.getOpportunities, filter) as Observable<IApiPaginatedResponse<IOpportunityRecord[]>>;
+  getOpportunities(filter: IOpportunitiesFilterRequest): Observable<IBaseApiResponse<IApiPaginatedResponse<IOpportunityRecord[]>>> {
+    return this.baseHttpService.get<IApiPaginatedResponse<IOpportunityRecord[]>, unknown>(API_ENDPOINTS.opportunities.getOpportunities, filter);
   }
 
-  getAdminOpportunities(filter: IOpportunitiesFilterRequest): Observable<IApiPaginatedResponse<IOpportunityRecord[]>> {
-    return this.baseHttpService.get<IOpportunityRecord[], unknown>(API_ENDPOINTS.opportunities.getAdminOpportunities, filter) as Observable<IApiPaginatedResponse<IOpportunityRecord[]>>;
+  getAdminOpportunities(filter: IOpportunitiesFilterRequest): Observable<IBaseApiResponse<IApiPaginatedResponse<IOpportunityRecord[]>>> {
+    return this.baseHttpService.get<IApiPaginatedResponse<IOpportunityRecord[]>, unknown>(API_ENDPOINTS.opportunities.getAdminOpportunities, filter);
   }
 }
