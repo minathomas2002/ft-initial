9521f029a7a522b02e79593eab5061edc91581a9
Mina Thomas <mina.thomas@flairstech.com>
2025-11-22 11:23:32 +0200

    (authentication) handle success false error

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -8,10 +8,20 @@ import { catchError, tap } from 'rxjs/operators';
 import { inject } from '@angular/core';
 import { throwError } from 'rxjs';
 import { ToasterService } from '../../../shared/services/toaster/toaster.service';
-
+/**
+ * Interceptor to handle errors from the API by display error messages in toaster.
+ * It handles both success and error responses from the API.
+ */
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toaster = inject(ToasterService);
   return next(req).pipe(
+    tap((response: any) => {
+      if(response?.body?.success === false) {
+        response?.body?.errors?.forEach((error: string) => {
+          toaster.error(error);
+        });
+      }
+    }),
     catchError((response: HttpErrorResponse) => {
       if (response.error?.success === false) {
         response.error?.errors?.forEach((error: string) => {
