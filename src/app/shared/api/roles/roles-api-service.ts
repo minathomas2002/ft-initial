821edfac9b167c44f66793a64a1f400d639f2b00
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-01 19:27:57 +0200

    Refactor RolesApiService: rename methods for clarity, add getRoleById and assignRoleToUser functionality

--- src/app/shared/api/roles/roles-api-service.ts
+++ src/app/shared/api/roles/roles-api-service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { BaseHttpService } from '../../services/Base-HTTP/base-Http.service';
 import { Observable } from 'rxjs';
-import { IBaseApiResponse, IRole } from '../../interfaces';
+import { IAssignRoleToUserRequest, IBaseApiResponse, IRole } from '../../interfaces';
 import { API_ENDPOINTS } from '../api-endpoints';
 
 @Injectable({
@@ -10,16 +10,23 @@ import { API_ENDPOINTS } from '../api-endpoints';
 export class RolesApiService {
   private readonly baseHttpService = inject(BaseHttpService);
 
-  getRoles(): Observable<IBaseApiResponse<IRole[]>> {
+  getAllRoles(): Observable<IBaseApiResponse<IRole[]>> {
     return this.baseHttpService.get<IRole[], unknown>(API_ENDPOINTS.roles.getRoles);
   }
 
-  getUserRoles(): Observable<IBaseApiResponse<IRole[]>> {
-    return this.baseHttpService.get<IRole[], unknown>(API_ENDPOINTS.roles.getUserRoles);
+  getSystemRoles(): Observable<IBaseApiResponse<IRole[]>> {
+    return this.baseHttpService.get<IRole[], unknown>(API_ENDPOINTS.roles.getSystemRoles);
   }
 
   getFilteredRoles(): Observable<IBaseApiResponse<IRole[]>> {
     return this.baseHttpService.get<IRole[], unknown>(API_ENDPOINTS.roles.getFilteredRoles);
   }
-}
 
+  getRoleById(id: string): Observable<IBaseApiResponse<IRole>> {
+    return this.baseHttpService.get<IRole, unknown>(`${API_ENDPOINTS.roles.getRoleById}/${id}`);
+  }
+
+  assignRoleToUser(request: IAssignRoleToUserRequest): Observable<IBaseApiResponse<void>> {
+    return this.baseHttpService.post<void, IAssignRoleToUserRequest, unknown>(`${API_ENDPOINTS.roles.assignRoleToUser}`, request);
+  }
+}
