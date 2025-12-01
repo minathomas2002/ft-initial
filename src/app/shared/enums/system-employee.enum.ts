ef7bc09708504831e8b221a08b7a3419292c9525
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-01 21:36:56 +0200

    Add system employee management interfaces, enums, and store: introduce employee status enums, detailed employee interfaces, and a store for managing employee data and actions.

---
+++ src/app/shared/enums/system-employee.enum.ts
@@ -0,0 +1,13 @@
+export enum EUserStatus {
+  ACTIVE = "active",
+  INACTIVE = "inactive",
+  DELEGATED = "delegated"
+}
+
+export enum EAdminUserActions {
+  EDIT = 1,
+  DEACTIVATE = 2,
+  VIEW = 3,
+  CHANGE_ROLE = 4,
+  DELETE = 5
+}
---
+++ src/app/shared/interfaces/system-employees.interface.ts
@@ -0,0 +1,84 @@
+import { EAdminUserActions } from "../enums";
+import { IFilterBase } from "./filter.interface";
+import { ISelectItem } from "./select-item.interface";
+
+export interface IEmployeeDateFromHR {
+  employeeID: string;
+  userName: string;
+  nameAr: string;
+  nameEn: string;
+  phoneNumber: string;
+  email: string;
+  isRealData: boolean;
+}
+
+export interface IActiveEmployee {
+  id: string;
+  userId: string;
+  nameAr: string;
+  nameEn: string;
+}
+
+export interface ISystemEmployeeDetails {
+  id: string;
+  employeeID: string;
+  nameEn: string;
+  nameAr: string;
+  email: string;
+  phoneNumber: string;
+  department: string;
+  roleId: string;
+  roleName: string;
+  roleCode: number;
+  active: boolean;
+  userId: string;
+  joinDate: string;
+
+}
+
+export interface ICreateSystemEmployeeRequest {
+  employeeID: string;
+  nameAr: string;
+  nameEn: string;
+  email: string;
+  phoneNumber: string;
+  roleId: string;
+}
+
+export interface ISystemEmployeeRecord {
+  id: string;
+  employeeID: string;
+  nameAr: string;
+  nameEn: string;
+  email: string;
+  phoneNumber: string;
+  department: string;
+  role: string;
+  roleCode: number;
+  joinDate: string;
+  status: string;
+  userId: string;
+  actions: EAdminUserActions[];
+}
+
+export type TSystemEmployeeSortingKeys = keyof ISystemEmployeeRecord;
+
+export interface ISystemEmployeeFilter extends IFilterBase<TSystemEmployeeSortingKeys> {
+  searchText?: string;
+  roleIds?: string[];
+  statusFilters?: ISelectItem[];
+}
+
+export interface ISystemEmployeeFilterRequest extends IFilterBase<TSystemEmployeeSortingKeys> {
+  searchText?: string;
+  roleIds?: number[];
+  statusFilters?: boolean[];
+}
+
+export interface IUpdateSystemEmployeeRequest {
+  id: string;
+  nameAr: string;
+  nameEn: string;
+  phoneNumber: string;
+  roleId: string;
+}
