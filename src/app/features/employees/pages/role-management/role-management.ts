ef2d304d3eed8e914f45f5e3a1dc96ea9c0c85b5
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-02 16:59:47 +0200

    Implement role transfer functionality: remove job number display, add actions column with transfer role dialog integration, and enhance role management component with new signals and methods for handling role transfers.

--- src/app/features/employees/pages/role-management/role-management.ts
+++ src/app/features/employees/pages/role-management/role-management.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { TableLayoutComponent } from 'src/app/shared/components/layout-components/table-layout/table-layout.component';
 import { TableSkeletonComponent } from 'src/app/shared/components/skeletons/table-skeleton/table-skeleton.component';
 import { RoleManagementFilters } from '../../components/role-management-filters/role-management-filters';
@@ -6,6 +6,7 @@ import {
   ITableHeaderItem,
   IRoleManagementAssignmentRecord,
   TRoleManagementAssignmentSortingKeys,
+  ITransferRoleRequest,
 } from 'src/app/shared/interfaces';
 import { RoleManagementStore } from 'src/app/shared/stores/system-employees/role-management-store';
 import { DataTableComponent } from 'src/app/shared/components/layout-components/data-table/data-table.component';
@@ -18,6 +19,10 @@ import { BaseTagComponent } from 'src/app/shared/components/base-components/base
 import { DatePipe } from '@angular/common';
 import { ERoles } from 'src/app/shared/enums';
 import { TooltipModule } from 'primeng/tooltip';
+import { RoleManagementActions } from '../../components/role-management-actions/role-management-actions';
+import { TransferRoleDialog } from '../../components/transfer-role-dialog/transfer-role-dialog';
+import { TransferRoleService } from '../../services/transfer-role/transfer-role-service';
+import { ToasterService } from 'src/app/shared/services/toaster/toaster.service';
 
 @Component({
   selector: 'app-role-management',
@@ -28,7 +33,9 @@ import { TooltipModule } from 'primeng/tooltip';
     RoleManagementFilters,
     BaseTagComponent,
     DataTableComponent,
-    TooltipModule
+    TooltipModule,
+    RoleManagementActions,
+    TransferRoleDialog
   ],
   templateUrl: './role-management.html',
   styleUrl: './role-management.scss',
@@ -47,11 +54,6 @@ export class RoleManagement implements OnInit {
         sortingKey: 'name_En',
       },
       {
-        label: this.i18nService.translate('users.table.jobNo') || 'Job No.',
-        isSortable: true,
-        sortingKey: 'jobId',
-      },
-      {
         label: this.i18nService.translate('users.table.email') || 'E-mail',
         isSortable: false
       },
@@ -60,16 +62,21 @@ export class RoleManagement implements OnInit {
         isSortable: false
       },
       {
-        label: this.i18nService.translate('users.table.assignedBy') || 'Assigned BY',
-        isSortable: false
+        label: this.i18nService.translate('users.table.assignedBy') || 'AssignedBy',
+        isSortable: true,
+        sortingKey: 'assignedBy',
       },
       {
-        label: this.i18nService.translate('users.table.assignedDate') || 'Assigned Date',
+        label: this.i18nService.translate('users.table.assignedDate') || 'assignedDate',
         isSortable: true,
         sortingKey: 'assignedDate',
       },
       {
         label: this.i18nService.translate('users.table.status') || 'Status',
+        isSortable: false
+      },
+      {
+        label: this.i18nService.translate('users.table.actions') || 'Actions',
         isSortable: false
       }
     ];
@@ -80,6 +87,10 @@ export class RoleManagement implements OnInit {
   totalRecords = computed(() => this.roleManagementStore.roleManagementCount());
   userStatusMapper = new UserStatusMapper(this.i18nService);
   userRoleMapper = new EmployeeRoleMapper(this.i18nService);
+  selectedItem = signal<IRoleManagementAssignmentRecord | null>(null);
+  transferRoleDialogVisible = signal<boolean>(false);
+  transferRoleService = inject(TransferRoleService);
+  toasterService = inject(ToasterService);
 
   ngOnInit(): void {
     this.filterService.applyFilter();
@@ -104,6 +115,28 @@ export class RoleManagement implements OnInit {
     // Return name based on current language
     const currentLang = this.i18nService.currentLanguage();
     return currentLang === 'ar' ? item.name_Ar : item.name_En;
+  }
+
+  onTransfer(item: IRoleManagementAssignmentRecord): void {
+    this.selectedItem.set(item);
+    this.transferRoleDialogVisible.set(true);
+  }
+
+  onConfirmTransferRole(): void {
+    const request: ITransferRoleRequest = {
+      currentUserId: this.selectedItem()?.userId!,
+      newUserId: this.transferRoleService.employee.value!,
+      roleCode: this.selectedItem()?.roleCode!
+    }
+    this.roleManagementStore.transferRole(request).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.filterService.applyFilter();
+          this.toasterService.success(this.i18nService.translate('users.messages.transferRoleSuccess'));
+          this.transferRoleDialogVisible.set(false);
+        }
+      }
+    })
   }
 }
 
