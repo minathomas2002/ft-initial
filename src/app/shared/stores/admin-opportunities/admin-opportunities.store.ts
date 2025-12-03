a73e7b875dea876cdb98e3f1a5bd257681653bcf
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-03 15:48:34 +0200

    Enhance opportunity category selection: add icon support in ISelectItem interface, update admin opportunities store with icons, and improve opportunity information form display.

--- src/app/shared/stores/admin-opportunities/admin-opportunities.store.ts
+++ src/app/shared/stores/admin-opportunities/admin-opportunities.store.ts
@@ -42,10 +42,12 @@ const initialState: {
   opportunityCategories: [{
     id: '1',
     name: 'Category 1',
+    icon: 'icon-bell'
   },
   {
     id: '2',
     name: 'Category 2',
+    icon: 'icon-home'
   }],
   statusOptions: [
     { id: EOpportunityStatus.PUBLISHED.toString(), name: 'opportunity.status.published' },
