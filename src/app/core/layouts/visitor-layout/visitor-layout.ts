69aa72975a0505570876e87a237d9bfb89a97115
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-04 15:56:54 +0200

    Remove TrimOnBlurDirective from multiple layouts and components to streamline imports and improve code clarity.

--- src/app/core/layouts/visitor-layout/visitor-layout.ts
+++ src/app/core/layouts/visitor-layout/visitor-layout.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { VisitorLayoutNavbar } from "./components/visitor-layout-navbar/visitor-layout-navbar";
 import { RouterOutlet } from '@angular/router';
 import { LayoutFooterComponent } from '../../../shared/components/layout-components/layout-footer/layout-footer.component';
-import { TrimOnBlurDirective } from 'src/app/shared/directives/trim-on-blur.directive';
 
 @Component({
   selector: 'app-visitor-layout',
-  imports: [VisitorLayoutNavbar, RouterOutlet, LayoutFooterComponent, TrimOnBlurDirective],
+  imports: [VisitorLayoutNavbar, RouterOutlet, LayoutFooterComponent],
   templateUrl: './visitor-layout.html',
   styleUrl: './visitor-layout.scss',
 })
