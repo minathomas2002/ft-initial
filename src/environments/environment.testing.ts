a2be0ef4f60f793375bfe24496efcffbd1ac55a1
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-04 17:11:37 +0200

    Update password policy UI: change small text class for improved styling and modify baseUrl in environment.testing.ts to remove trailing slash for consistency.

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://10.15.1.68/api/',
+  baseUrl: 'http://10.15.1.68/api',
   apiVersion: 'v1',
   appName: 'Benaa',
   enableDebug: true,
