4fefb24e2d0211442213b9a5712bf1064969aa6c
Mina Thomas <mina.thomas@flairstech.com>
2025-12-08 11:24:46 +0200

    handle routing to admin login

--- src/app/features/dashboard/pages/dashboard-view/dashboard-view.ts
+++ src/app/features/dashboard/pages/dashboard-view/dashboard-view.ts
@@ -1,16 +1,24 @@
-import { Component, inject } from '@angular/core';
-import { HelloWorld } from '../../components/hello-world/hello-world';
-import { HelloWorldService } from '../../services/hello-world';
-import { HelloWorldGreeter } from '../../class/hello-world';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ERoutes } from 'src/app/shared/enums';
+import { ERoles } from 'src/app/shared/enums/roles.enum';
+import { RoleService } from 'src/app/shared/services/role/role-service';
 
 @Component({
   selector: 'app-dashboard-view',
-  imports: [HelloWorld],
+  imports: [],
   templateUrl: './dashboard-view.html',
   styleUrl: './dashboard-view.scss',
-  providers:[HelloWorldService]
+  providers:[]
 })
-export class DashboardView {
-  private helloWorldService = inject(HelloWorldService);
-  protected helloWorld = new HelloWorldGreeter('John');
+export class DashboardView implements OnInit {
+  
+  private readonly roleService = inject(RoleService);
+  router = inject(Router);
+
+  ngOnInit(): void {
+    if(this.roleService.hasAnyRoleSignal([ERoles.ADMIN])()) {
+      this.router.navigate([ERoutes.opportunities, ERoutes.admin]);
+    }
+  }
 }
