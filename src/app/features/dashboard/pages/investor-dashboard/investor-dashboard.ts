c1a3337bd18bbea578f813e299f5921e568cd413
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-08 20:28:22 +0200

    feat: integrate plan terms and conditions dialog into investor dashboard with visibility control

--- src/app/features/dashboard/pages/investor-dashboard/investor-dashboard.ts
+++ src/app/features/dashboard/pages/investor-dashboard/investor-dashboard.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableLayoutComponent } from 'src/app/shared/components/layout-components/table-layout/table-layout.component';
+import { PlanTermsAndConditionsDialog } from 'src/app/shared/components/plans/plan-terms-and-conditions-dialog/plan-terms-and-conditions-dialog';
 
 @Component({
   selector: 'app-investor-dashboard',
   imports: [
     TableLayoutComponent,
-    ButtonModule
+    ButtonModule,
+    PlanTermsAndConditionsDialog
   ],
   templateUrl: './investor-dashboard.html',
   styleUrl: './investor-dashboard.scss',
 })
 export class InvestorDashboard {
+  planTermsAndConditionsDialogVisibility = signal(false);
 
+  onUserReadAndApproved() {
+    console.log('user read and approved');
+    this.planTermsAndConditionsDialogVisibility.set(false);
+  };
+
+  onUserCancelTermsAndConditionsDialog() {
+    console.log('user canceled terms and conditions dialog');
+  };
 }
