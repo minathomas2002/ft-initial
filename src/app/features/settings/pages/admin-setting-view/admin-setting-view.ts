1b2fdf27d4cbb893e007a95b87ddfc7ec99c6eba
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-09 10:47:35 +0200

    add Setting route

---
+++ src/app/features/settings/pages/admin-setting-view/admin-setting-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-setting-view',
+  imports: [],
+  templateUrl: './admin-setting-view.html',
+  styleUrl: './admin-setting-view.scss',
+})
+export class AdminSettingView {
+
+}
