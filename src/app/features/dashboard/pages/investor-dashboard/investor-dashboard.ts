812891153906ddfa6267f2ac1f1b84a68200be06
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-09 16:32:25 +0200

    feat: enhance investor dashboard with conditional dialogs for plan terms and new plan confirmation

--- src/app/features/dashboard/pages/investor-dashboard/investor-dashboard.ts
+++ src/app/features/dashboard/pages/investor-dashboard/investor-dashboard.ts
@@ -1,27 +1,43 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, computed, inject, output, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableLayoutComponent } from 'src/app/shared/components/layout-components/table-layout/table-layout.component';
+import { NewPlanDialog } from 'src/app/shared/components/plans/new-plan-dialog/new-plan-dialog';
 import { PlanTermsAndConditionsDialog } from 'src/app/shared/components/plans/plan-terms-and-conditions-dialog/plan-terms-and-conditions-dialog';
+import { EOpportunityType } from 'src/app/shared/enums';
+import { PlanStore } from 'src/app/shared/stores/plan/plan.store';
 
 @Component({
   selector: 'app-investor-dashboard',
   imports: [
     TableLayoutComponent,
     ButtonModule,
-    PlanTermsAndConditionsDialog
+    PlanTermsAndConditionsDialog,
+    NewPlanDialog
   ],
   templateUrl: './investor-dashboard.html',
   styleUrl: './investor-dashboard.scss',
 })
 export class InvestorDashboard {
   planTermsAndConditionsDialogVisibility = signal(false);
+  newPlanDialogVisibility = signal(false);
+
+  private readonly planStore = inject(PlanStore);
+  newPlanOpportunityType = computed(() => this.planStore.newPlanOpportunityType());
 
   onUserReadAndApproved() {
-    console.log('user read and approved');
     this.planTermsAndConditionsDialogVisibility.set(false);
+    this.planStore.resetNewPlanOpportunityType();
+    this.newPlanDialogVisibility.set(true);
   };
 
-  onUserCancelTermsAndConditionsDialog() {
-    console.log('user canceled terms and conditions dialog');
-  };
+
+  onUserConfirmNewPlanDialog() {
+    this.planStore.getActiveOpportunityLookUps();
+    this.newPlanDialogVisibility.set(false);
+    if (this.newPlanOpportunityType() && this.newPlanOpportunityType()! === EOpportunityType.MATERIAL) {
+      console.log('material');
+    } else {
+      console.log('service');
+    }
+  }
 }
