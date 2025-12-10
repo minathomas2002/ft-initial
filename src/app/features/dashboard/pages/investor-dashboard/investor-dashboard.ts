c3443b6e68fd03f5155359b9b166208e2687c374
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-10 15:16:48 +0200

    feat: integrate materials sourcing plan wizard into investor dashboard with conditional visibility

--- src/app/features/dashboard/pages/investor-dashboard/investor-dashboard.ts
+++ src/app/features/dashboard/pages/investor-dashboard/investor-dashboard.ts
@@ -1,6 +1,8 @@
 import { Component, computed, inject, output, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { take } from 'rxjs';
 import { TableLayoutComponent } from 'src/app/shared/components/layout-components/table-layout/table-layout.component';
+import { MaterialsSourcingPlanWizard } from 'src/app/shared/components/plans/materials-sourcing-plan-wizard/materials-sourcing-plan-wizzard/materials-sourcing-plan-wizard';
 import { NewPlanDialog } from 'src/app/shared/components/plans/new-plan-dialog/new-plan-dialog';
 import { PlanTermsAndConditionsDialog } from 'src/app/shared/components/plans/plan-terms-and-conditions-dialog/plan-terms-and-conditions-dialog';
 import { EOpportunityType } from 'src/app/shared/enums';
@@ -12,7 +14,8 @@ import { PlanStore } from 'src/app/shared/stores/plan/plan.store';
     TableLayoutComponent,
     ButtonModule,
     PlanTermsAndConditionsDialog,
-    NewPlanDialog
+    NewPlanDialog,
+    MaterialsSourcingPlanWizard
   ],
   templateUrl: './investor-dashboard.html',
   styleUrl: './investor-dashboard.scss',
@@ -20,6 +23,7 @@ import { PlanStore } from 'src/app/shared/stores/plan/plan.store';
 export class InvestorDashboard {
   planTermsAndConditionsDialogVisibility = signal(false);
   newPlanDialogVisibility = signal(false);
+  materialsSourcingPlanWizardVisibility = signal(false);
 
   private readonly planStore = inject(PlanStore);
   newPlanOpportunityType = computed(() => this.planStore.newPlanOpportunityType());
@@ -32,10 +36,10 @@ export class InvestorDashboard {
 
 
   onUserConfirmNewPlanDialog() {
-    this.planStore.getActiveOpportunityLookUps();
+    this.planStore.getActiveOpportunityLookUps().pipe(take(1)).subscribe();
     this.newPlanDialogVisibility.set(false);
     if (this.newPlanOpportunityType() && this.newPlanOpportunityType()! === EOpportunityType.MATERIAL) {
-      console.log('material');
+      this.materialsSourcingPlanWizardVisibility.set(true);
     } else {
       console.log('service');
     }
