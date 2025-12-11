d735d8cedcc888fc5fae3173558d105952b4a2d8
Mina Thomas <mina.thomas@flairstech.com>
2025-12-11 09:53:53 +0200

    fix issues

--- src/app/shared/components/utility-components/empty-records/empty-records.component.ts
+++ src/app/shared/components/utility-components/empty-records/empty-records.component.ts
@@ -15,7 +15,7 @@ export class EmptyRecordsComponent {
 	iconClass = input<string>();
 	subTitleClass = input<string>();
 	subtitle = input<string>(
-		`We couldn't find any data matching your filters. Try adjusting the criteria or date to see results.`,
+		`We couldn't find any data matching your filters. Try adjusting the criteria to see results.`,
 	);
 	icon = input<string>("icon-search");
 }
