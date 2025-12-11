da7e49c26f04ca8c6c3643adf74aa4b84748eca1
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-11 10:41:13 +0200

    IMP email form component

---
+++ src/app/features/settings/components/admin-notification-email-form/admin-notification-email-form.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { EmailNotificationFormService } from '../../services/email-notification-form-service';
+import { TranslatePipe } from "../../../../shared/pipes/translate.pipe";
+import { Tooltip } from "primeng/tooltip";
+import { Divider } from "primeng/divider";
+import { BaseLabelComponent } from "src/app/shared/components/base-components/base-label/base-label.component";
+import { ToggleSwitch } from "primeng/toggleswitch";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-admin-notification-email-form',
+  imports: [TranslatePipe, Tooltip, Divider, BaseLabelComponent, ToggleSwitch,FormsModule],
+  templateUrl: './admin-notification-email-form.html',
+  styleUrl: './admin-notification-email-form.scss',
+})
+export class AdminNotificationEmailForm {
+
+    formService = input.required<EmailNotificationFormService>();
+
+}
