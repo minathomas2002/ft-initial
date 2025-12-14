6d5244a86752b361d694d7655f8398769eaaf3d7
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-14 18:23:30 +0200

    Refactor SettingCard component by removing unused Tooltip import and cleaning up input/output property formatting.

--- src/app/features/settings/components/setting-card/setting-card.ts
+++ src/app/features/settings/components/setting-card/setting-card.ts
@@ -1,18 +1,17 @@
-import { Component, input, output, signal } from '@angular/core';
-import { Tooltip } from "primeng/tooltip";
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-setting-card',
-  imports: [Tooltip],
+  imports: [],
   templateUrl: './setting-card.html',
   styleUrl: './setting-card.scss',
 })
 export class SettingCard {
 
-  title= input.required<string>();
-  description= input.required<string>();
-  linkText= input.required<string>();
-  icon= input.required<string>();
-  
+  title = input.required<string>();
+  description = input.required<string>();
+  linkText = input.required<string>();
+  icon = input.required<string>();
+
   onClick = output();
 }
