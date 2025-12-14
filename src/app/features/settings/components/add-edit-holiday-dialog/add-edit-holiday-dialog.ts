d856afdec5f85cb74e4bac3af47a380e84eef11f
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-14 15:17:46 +0200

    Create add dialog

---
+++ src/app/features/settings/components/add-edit-holiday-dialog/add-edit-holiday-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-holiday-dialog',
+  imports: [],
+  templateUrl: './add-edit-holiday-dialog.html',
+  styleUrl: './add-edit-holiday-dialog.scss',
+})
+export class AddEditHolidayDialog {
+
+}
