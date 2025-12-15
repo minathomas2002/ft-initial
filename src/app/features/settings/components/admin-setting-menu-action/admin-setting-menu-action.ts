f404e7adc27944563b7cb803d654b4739667ddd7
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-15 12:35:07 +0200

    create menu admin setting component

---
+++ src/app/features/settings/components/admin-setting-menu-action/admin-setting-menu-action.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-setting-menu-action',
+  imports: [],
+  templateUrl: './admin-setting-menu-action.html',
+  styleUrl: './admin-setting-menu-action.scss',
+})
+export class AdminSettingMenuAction {
+
+}
