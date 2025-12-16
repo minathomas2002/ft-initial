a69ee5abffe3c2f754785b8da87d1f4bf3315fe4
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-16 11:10:11 +0200

    add  assign form service

--- src/app/features/plans/services/assign-reassign-form/assign-reassign-form-service.ts
+++ src/app/features/plans/services/assign-reassign-form/assign-reassign-form-service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
@@ -6,6 +7,22 @@ import { Injectable } from '@angular/core';
 export class AssignReassignFormService {
   
   //IAssignRequest
+  private fb = inject(FormBuilder);
 
+  //declare strongly form
+  readonly form  : FormGroup<{
+    planId: FormControl<string | null>,
+    employeeId: FormControl<string | null>
+  }> = this.fb.group({
+    planId: this.fb.control<string | null>( null,[Validators.required]),
+    employeeId: this.fb.control<string | null> (null, [Validators.required])
+  });
+
+  get planId(){return this.form.controls.planId}
+  get employeeId() { return this.form.controls.employeeId};
   
+
+  resetForm(){
+    this.form.reset();
+  }
 }
