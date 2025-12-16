ea208a64156c476d8402f5c58ef1200e5f39a802
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-16 11:09:48 +0200

    imp layout assign

--- src/app/features/plans/components/assign-reassign-mauale-employee/assign-reassign-mauale-employee.ts
+++ src/app/features/plans/components/assign-reassign-mauale-employee/assign-reassign-mauale-employee.ts
@@ -1,9 +1,18 @@
-import { Component, input, model, output } from '@angular/core';
+import { Component, inject, input, model, output } from '@angular/core';
 import { IPlanRecord } from 'src/app/shared/interfaces';
+import { BaseDialogComponent } from "src/app/shared/components/base-components/base-dialog/base-dialog.component";
+import { AssignReassignFormService } from '../../services/assign-reassign-form/assign-reassign-form-service';
+import { TranslatePipe } from "../../../../shared/pipes/translate.pipe";
+import { SystemEmployeesStore } from 'src/app/shared/stores/system-employees/system-employees.store';
+import { take } from 'rxjs';
+import { BaseLabelComponent } from "src/app/shared/components/base-components/base-label/base-label.component";
+import { Select } from "primeng/select";
+import { ReactiveFormsModule } from '@angular/forms';
+import { BaseErrorComponent } from "src/app/shared/components/base-components/base-error/base-error.component";
 
 @Component({
   selector: 'app-assign-reassign-mauale-employee',
-  imports: [],
+  imports: [BaseDialogComponent, TranslatePipe, BaseLabelComponent, Select, ReactiveFormsModule, BaseErrorComponent],
   templateUrl: './assign-reassign-mauale-employee.html',
   styleUrl: './assign-reassign-mauale-employee.scss',
 })
@@ -13,14 +22,28 @@ export class AssignReassignMaualeEmployee {
   onSuccess = output<void>();
   isReassignMode = input<boolean>(false);
   planRecord = model<IPlanRecord|null>(null);
-
+  formService = inject(AssignReassignFormService);
+  systemEmployeesStore = inject(SystemEmployeesStore);
+  activeSystemEmployees = this.systemEmployeesStore.activeEmployees;
 
   ngOnInit(){
     this.loadActiveEmployees();
   }
 
+  onConfirm(){
+    console.log("apply assign / reassign ");
+    
+  }
+
+  resetForm(){
+    this.formService.resetForm();
+  }
+
   loadActiveEmployees(){
-    // load active users by plan id 
+    // To Do change endpoint name 
+    this.systemEmployeesStore.getActiveEmployeesForPlans(this.planRecord()?.id!)
+          .pipe(take(1))
+          .subscribe();
 
   }
 }
