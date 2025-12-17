06e6fc057c5d266cc003e53574b7a6efc9dd00bf
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-17 17:36:24 +0200

    Refactor PlanApiService for improved code readability and add saveAsDraftProductLocalizationPlan method. This update includes formatting adjustments for consistency and introduces a new API endpoint for saving draft product localization plans.

--- src/app/shared/api/plans/plan-api-service.ts
+++ src/app/shared/api/plans/plan-api-service.ts
@@ -3,26 +3,28 @@ import { BaseHttpService } from '../../services/Base-HTTP/base-Http.service';
 import { Observable } from 'rxjs';
 import { IActiveEmployee, IAssignRequest, IBaseApiResponse } from '../../interfaces';
 import { API_ENDPOINTS } from '../api-endpoints';
+import { IProductLocalizationPlanRequest } from '../../interfaces/plans.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlanApiService {
-private readonly baseHttpService = inject(BaseHttpService);
+  private readonly baseHttpService = inject(BaseHttpService);
 
-
-
-  
   getActiveEmployeesForPlans(planId: string): Observable<IBaseApiResponse<IActiveEmployee[]>> {
-    return this.baseHttpService.get<IActiveEmployee[], string>(API_ENDPOINTS.plans.getActiveEmployeesWithPlans+ planId);
+    return this.baseHttpService.get<IActiveEmployee[], string>(API_ENDPOINTS.plans.getActiveEmployeesWithPlans + planId);
   }
 
   assignEmployeeToPlan(req: IAssignRequest): Observable<IBaseApiResponse<boolean>> {
-    return this.baseHttpService.post<boolean,IAssignRequest, unknown>(API_ENDPOINTS.plans.assign, req);
+    return this.baseHttpService.post<boolean, IAssignRequest, unknown>(API_ENDPOINTS.plans.assign, req);
   }
 
-   reassignEmployeeToPlan(req: IAssignRequest): Observable<IBaseApiResponse<boolean>> {
-    return this.baseHttpService.post<boolean,IAssignRequest, unknown>(API_ENDPOINTS.plans.reassign, req);
+  reassignEmployeeToPlan(req: IAssignRequest): Observable<IBaseApiResponse<boolean>> {
+    return this.baseHttpService.post<boolean, IAssignRequest, unknown>(API_ENDPOINTS.plans.reassign, req);
   }
-  
+
+  saveAsDraftProductLocalizationPlan(req: FormData): Observable<IBaseApiResponse<boolean>> {
+    return this.baseHttpService.post<boolean, FormData, unknown>(API_ENDPOINTS.plans.saveAsDraftProductLocalizationPlan, req);
+  }
+
 }
