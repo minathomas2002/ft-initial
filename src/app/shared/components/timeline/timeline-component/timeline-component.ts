3a475c651b14c1c7419e5c961dcecc5615d5f57a
Marwa Mohamed <MAR.Mohamed@diyarme.com>
2025-12-23 18:19:18 +0200

    create timeline nody component

---
+++ src/app/shared/components/timeline/timeline-component/timeline-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-component',
+  imports: [],
+  templateUrl: './timeline-component.html',
+  styleUrl: './timeline-component.scss',
+})
+export class TimelineComponent {
+
+}
