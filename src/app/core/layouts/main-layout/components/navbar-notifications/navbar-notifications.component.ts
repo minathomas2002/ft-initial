d4c260e11a274239d69616a48bf45ef969484afc
Mina Thomas <mina.thomas@flairstech.com>
2025-12-24 09:37:36 +0200

    initial setup for signal r connection

--- src/app/core/layouts/main-layout/components/navbar-notifications/navbar-notifications.component.ts
+++ src/app/core/layouts/main-layout/components/navbar-notifications/navbar-notifications.component.ts
@@ -3,6 +3,7 @@ import {
 	ChangeDetectionStrategy,
 	Component,
 	OnInit,
+	OnDestroy,
 	ViewChild,
 	effect,
 	inject,
@@ -14,6 +15,8 @@ import { MenuModule } from "primeng/menu";
 import { OverlayBadgeModule } from "primeng/overlaybadge";
 import { Popover, PopoverModule } from "primeng/popover";
 import { ERoutes } from "../../../../../shared/enums";
+import { NotificationHubService } from "../../../../../shared/services/signalr/notification-hub.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
 	selector: "app-navbar-notifications",
@@ -29,8 +32,9 @@ import { ERoutes } from "../../../../../shared/enums";
 	styleUrl: "./navbar-notifications.component.scss",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarNotificationsComponent implements OnInit, AfterViewInit {
-
+export class NavbarNotificationsComponent implements OnInit, AfterViewInit, OnDestroy {
+	private readonly notificationHubService = inject(NotificationHubService);
+	private readonly destroy$ = new Subject<void>();
 
 	lastSync: Date = new Date();
 
@@ -54,10 +58,35 @@ export class NavbarNotificationsComponent implements OnInit, AfterViewInit {
 	get ERoutes() {
 		return ERoutes;
 	}
+
 	ngOnInit(): void {
-		// this.notificationsActionsService.getAll().subscribe();
+		// Start SignalR connection
+		this.notificationHubService
+			.startConnection()
+			.catch((error) => {
+				console.error("Failed to start SignalR connection:", error);
+			});
+
+		// Subscribe to ReceiveNotification event
+		this.notificationHubService
+			.onReceiveNotification()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((notification) => {
+				console.log("Notification received in component:", notification);
+				// Handle notification here
+				// You can add your notification handling logic here
+			});
 	}
+
 	ngAfterViewInit(): void {
 		// this.notificationsActionsService.setPopoverRef(this.popoverRef);
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next();
+		this.destroy$.complete();
+		// Note: We're not stopping the connection here because it might be used by other components
+		// If you want to stop it when this component is destroyed, uncomment the line below:
+		// this.notificationHubService.stopConnection();
 	}
 }
