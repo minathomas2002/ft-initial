4532613d2534e5ee805ec9e6ce1182fc21f58417
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2025-12-30 15:31:31 +0200

    Update file upload placeholder text: clarify accepted file types by removing archive formats from the upload instructions. Refactor imports in identify-user component to remove unused Tooltip module for cleaner code.

--- src/app/shared/components/utility-components/identify-user/identify-user.component.ts
+++ src/app/shared/components/utility-components/identify-user/identify-user.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import { AvatarModule } from "primeng/avatar";
-import { Tooltip } from "primeng/tooltip";
 @Component({
 	selector: "app-identify-user",
-	imports: [AvatarModule, Tooltip],
+	imports: [AvatarModule,],
 	templateUrl: "./identify-user.component.html",
 	styleUrl: "./identify-user.component.scss",
 	changeDetection: ChangeDetectionStrategy.OnPush,
