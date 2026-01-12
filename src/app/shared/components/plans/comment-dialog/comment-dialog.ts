2a241207dc6159d2550545eb8d798f1ee9685b4c
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-01-12 11:11:40 +0200

    Add CommentDialog component with HTML, SCSS, and TypeScript files; implement reactive form for comment input and success notification on submission.

---
+++ src/app/shared/components/plans/comment-dialog/comment-dialog.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, inject, model } from '@angular/core';
+import { BaseDialogComponent } from '../../base-components/base-dialog/base-dialog.component';
+import { BaseLabelComponent } from '../../base-components/base-label/base-label.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
+import { TrimOnBlurDirective } from 'src/app/shared/directives';
+import { BaseErrorMessages } from '../../base-components/base-error-messages/base-error-messages';
+import { ToasterService } from 'src/app/shared/services/toaster/toaster.service';
+
+@Component({
+  selector: 'app-comment-dialog',
+  imports: [
+    BaseDialogComponent,
+    BaseLabelComponent,
+    ReactiveFormsModule,
+    TextareaModule,
+    TrimOnBlurDirective,
+    BaseErrorMessages
+  ],
+  templateUrl: './comment-dialog.html',
+  styleUrl: './comment-dialog.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CommentDialog {
+  visible = model<boolean>(false);
+  private readonly fb = inject(FormBuilder);
+  private readonly toaster = inject(ToasterService);
+  commentFormControl = this.fb.control('', [Validators.required, Validators.maxLength(255)]);
+
+  onConfirm(): void {
+    if (this.commentFormControl.valid) {
+      this.toaster.success('Comment added successfully');
+      this.visible.set(false);
+    }
+  }
+}
