3cd95a3f51d7ec87e0a0b29c8d4ed398a7b45541
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-01-13 21:48:28 +0200

    Refactor comment state component to enhance comment management with improved editing, saving, and deletion functionalities. Update template to reflect new comment phases and integrate success notifications for user actions.

--- src/app/shared/components/plans/comment-state-component/comment-state-component.ts
+++ src/app/shared/components/plans/comment-state-component/comment-state-component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, input, linkedSignal, model, output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, input, linkedSignal, model, output } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { CommentDialog } from '../comment-dialog/comment-dialog';
+import { TCommentPhase } from '../plan-localization/product-localization-plan-wizard/product-localization-plan-wizard';
+import { ToasterService } from 'src/app/shared/services/toaster/toaster.service';
 
 @Component({
   selector: 'app-comment-state-component',
@@ -20,12 +22,35 @@ export class CommentStateComponent {
   commentsCount = input<number>(0);
   checked = linkedSignal<boolean>(() => this.commentsCount() > 0);
   showCommentDialog = model<boolean>(false);
-  comment = output<string>();
-  hasComment = model<boolean>(false)
+  commentAdded = output();
+  commentFormControl = input.required<FormControl<string>>();
+  commentPhase = model<TCommentPhase>();
+  startEditing = output();
+  deleteComments = output();
+  saveComment = output();
+  toaster = inject(ToasterService);
 
+  onCommentAdded() {
+    this.commentPhase.set('viewing');
+    this.commentFormControl().disable();
+  }
 
-  onCommentChanges($event: string) {
-    this.comment.emit($event);
-    this.hasComment.set(true);
+  onDeleteComments() {
+    this.commentPhase.set('none');
+    this.commentFormControl().reset();
+    this.toaster.success('Comments deleted successfully');
+    this.deleteComments.emit();
+  }
+
+  onStartEditing() {
+    this.commentPhase.set('editing');
+    this.commentFormControl()!.enable();
+  }
+
+  onSaveComment() {
+    this.saveComment.emit();
+    this.commentPhase.set('viewing');
+    this.commentFormControl().disable();
+    this.toaster.success('Comment saved successfully');
   }
 }
