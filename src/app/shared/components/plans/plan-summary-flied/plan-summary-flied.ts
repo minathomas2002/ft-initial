d346c6ccfe5a9bf3546dc9a452a5e1203c0e9185
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-01-30 18:01:01 +0200

    feat: Update PlanSummaryFlied component for improved resolution and difference display
    - Modified the HTML template to conditionally render the current value when the field is resolved, enhancing user feedback.
    - Adjusted the logic for displaying differences and comments based on the new `isResolved` and `showDifference` properties, improving clarity in the summary presentation.
    - Cleaned up TypeScript imports and removed unused computed properties for better performance and maintainability.

--- src/app/shared/components/plans/plan-summary-flied/plan-summary-flied.ts
+++ src/app/shared/components/plans/plan-summary-flied/plan-summary-flied.ts
@@ -1,8 +1,6 @@
-import { CommonModule, JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TooltipModule } from 'primeng/tooltip';
 import { IPlanSummaryField } from 'src/app/shared/interfaces/plans.interface';
-import { PlanStore } from 'src/app/shared/stores/plan/plan.store';
 
 @Component({
   selector: 'app-plan-summary-flied',
@@ -12,10 +10,6 @@ import { PlanStore } from 'src/app/shared/stores/plan/plan.store';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlanSummaryFlied {
-  planStore = inject(PlanStore);
   planSummaryField = input.required<IPlanSummaryField>();
-  showDifference = computed(() =>
-    this.planStore.wizardMode() === 'resubmit' &&
-    this.planSummaryField().beforeValue !== this.planSummaryField().currantValue
-  );
 }
+
