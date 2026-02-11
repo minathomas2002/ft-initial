891b1c22edeab145fdb118742ca7b21e6b803ce2
Mina Thomas <mina.thomas@flairstech.com>
2026-02-11 16:35:19 +0200

    fix filtering plans issues

--- src/app/features/plans/components/investor-plans-filter/investor-plans-filter.ts
+++ src/app/features/plans/components/investor-plans-filter/investor-plans-filter.ts
@@ -56,7 +56,7 @@ export class InvestorPlansFilter implements OnInit {
     this.listenToQueryParamChanges();
   }
 
-  private listenToQueryParamChanges() {
+  private listenToQueryParamChanges() {    
     this.route.queryParams
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(queryParams => {
