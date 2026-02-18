26a93dc13b0806125cb2fdb49606e2c00b346bd0
Amr Gamal <amgamal@kashier.io>
2026-02-18 15:39:16 +0200

    refactor: enhance timeline visibility logic and improve code structure in localization wizards

--- src/app/shared/components/plans/service-localication/service-localization-plan-wizard/service-localization-plan-wizard.ts
+++ src/app/shared/components/plans/service-localication/service-localization-plan-wizard/service-localization-plan-wizard.ts
@@ -138,7 +138,13 @@ export class ServiceLocalizationPlanWizard extends BasePlanWizard implements OnI
   mode = this.planStore.wizardMode;
   planId = this.planStore.selectedPlanId;
   canOpenTimeline = computed(() => {
-    return (this.visibility() && (this.mode() == 'view' || this.mode() == 'Review' || this.mode() == 'resubmit') && this.planStatus() !== null && this.planStatus() !== EInvestorPlanStatus.DRAFT && this.activeStep() < this.stepsWithId().length)
+    return (
+      this.visibility() &&
+      (this.mode() === 'view' || this.mode() === 'Review' || this.mode() === 'resubmit') &&
+      this.planStatus() !== null &&
+      (this.isInvestorPersona() ? this.planStatus() !== EInvestorPlanStatus.DRAFT : true) &&
+      this.activeStep() < this.stepsWithId().length
+    )
   });
 
   // Submission confirmation modal
