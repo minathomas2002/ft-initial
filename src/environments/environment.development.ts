52c1249855a212a22b09054161de1d3261685a05
Mina Thomas <mina.thomas@flairstech.com>
2026-02-22 17:08:02 +0200

    update dev url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@
 export const environment = {
 	production: false,
 	//baseUrl: 'http://10.15.1.68:8090/api',  
-	baseUrl: 'https://localhost:7026/api',
+	baseUrl: 'http://192.168.50.198:8052/api',
 	apiVersion: 'v1',
 	appName: 'BENA - DEV',
 	enableDebug: true,
