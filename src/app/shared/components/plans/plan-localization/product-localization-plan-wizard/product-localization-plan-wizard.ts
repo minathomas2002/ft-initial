d5b4f9c11bf29cbffdb471e55971e7a345c90d77
Amr Gamal <amgamal@kashier.io>
2026-02-22 17:47:40 +0200

    Refactor approval logic to remove DEPTManager status checks in localization wizards

--- src/app/shared/components/plans/plan-localization/product-localization-plan-wizard/product-localization-plan-wizard.ts
+++ src/app/shared/components/plans/plan-localization/product-localization-plan-wizard/product-localization-plan-wizard.ts
@@ -558,7 +558,7 @@ export class ProductLocalizationPlanWizard extends BasePlanWizard implements OnD
   });
 
   canApproveOrReject = computed(() => {
-    return (![EInternalUserPlanStatus.ReturnedByDV, EInternalUserPlanStatus.ReturnedByDEPTManager].includes(this.planStatus() as EInternalUserPlanStatus))
+    return (![EInternalUserPlanStatus.ReturnedByDV].includes(this.planStatus() as EInternalUserPlanStatus))
       && ((this.step1CommentPhase() === 'none' && this.step2CommentPhase() === 'none' && this.step3CommentPhase() === 'none' && this.step4CommentPhase() === 'none')
         || (!this.hasSelectedFields() &&
           !this.hasComments()))
@@ -572,7 +572,7 @@ export class ProductLocalizationPlanWizard extends BasePlanWizard implements OnD
     // // Check if any step has saved comments
     const planComments = this.planStore.planComments()?.comments ?? [];
     const currentUserPageComments = this.planStore.currentUserPageComments();
-    const returnedByManagerStatus = [EInternalUserPlanStatus.ReturnedByDEPTManager, EInternalUserPlanStatus.ReturnedByDV];
+    const returnedByManagerStatus = [EInternalUserPlanStatus.ReturnedByDV];
 
     if (returnedByManagerStatus.includes(this.planStatus() as EInternalUserPlanStatus)) {
       return planComments.every(comment => currentUserPageComments.includes(comment.pageTitleForTL));
