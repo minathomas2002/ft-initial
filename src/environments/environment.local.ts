f44aa8c116ba57ebef8f6eacbae97928be26a8ff
Amr Gamal <amgamal@kashier.io>
2026-02-23 12:38:06 +0200

    Fix baseUrl formatting in local environment configuration

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:7026/api/',
+  baseUrl: 'https://localhost:7026/api',
   apiVersion: 'v1',
   appName: 'Benaa',
   enableDebug: true,
