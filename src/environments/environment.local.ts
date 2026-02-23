c8fa52ae3d3945484eb64039c9e00ed86681d4e8
Amr Gamal <amgamal@kashier.io>
2026-02-23 12:34:35 +0200

    Add new local environment configuration for running projects locally separated from servers

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://localhost:7026/api/',
+  apiVersion: 'v1',
+  appName: 'Benaa',
+  enableDebug: true,
+  secDomain: 'http://localhost:4200'//should be updated with sec internal domain
+};
+
