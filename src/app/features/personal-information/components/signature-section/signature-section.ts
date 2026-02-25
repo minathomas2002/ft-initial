64bb866e574cd97c82c0562526f3972e42c06d40
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-02-25 16:08:04 +0200

    Implement validation for personal information fields to enhance user input handling

---
+++ src/app/features/personal-information/components/signature-section/signature-section.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signature-section',
+  imports: [],
+  templateUrl: './signature-section.html',
+  styleUrl: './signature-section.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignatureSection { }
