211291ed294f884649ceba06a76190b68db3942b
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-02-25 16:07:57 +0200

    Add conditional rendering for signature section in personal information view

--- src/app/features/personal-information/pages/personal-information-view/personal-information-view.ts
+++ src/app/features/personal-information/pages/personal-information-view/personal-information-view.ts
@@ -1,7 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { SectionsLayout } from 'src/app/shared/components/layout-components/sections-layout/sections-layout';
 import { PersonalInformationSection } from '../../components/personal-information-section/personal-information-section';
 import { UserImageSection } from '../../components/user-image-section/user-image-section';
+import { RoleService } from 'src/app/shared/services/role/role-service';
+import { ERoles } from 'src/app/shared/enums';
+import { SignatureSection } from '../../components/signature-section/signature-section';
 
 @Component({
   selector: 'app-personal-information-view',
@@ -9,9 +12,15 @@ import { UserImageSection } from '../../components/user-image-section/user-image
     SectionsLayout,
     UserImageSection,
     PersonalInformationSection,
+    SignatureSection,
   ],
   templateUrl: './personal-information-view.html',
   styleUrl: './personal-information-view.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PersonalInformationView { }
+export class PersonalInformationView {
+  private roleService = inject(RoleService);
+  protected canViewSignature = computed(() => this.roleService.hasAnyRoleSignal(
+    [ERoles.INVESTOR, ERoles.DEPARTMENT_MANAGER])
+    ());
+}
