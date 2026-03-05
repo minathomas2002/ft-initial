73e53b58e830e38271208bd5ac8bb1c4d6ea2dba
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-03-05 12:34:21 +0200

    Implement signature fetching in ApproveRejectDialogComponent

--- src/app/shared/components/utility-components/approve-reject-dialog/approve-reject-dialog.component.ts
+++ src/app/shared/components/utility-components/approve-reject-dialog/approve-reject-dialog.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed, input, model, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, input, model, output } from '@angular/core';
 import { BaseDialogComponent } from '../../base-components/base-dialog/base-dialog.component';
 import { FormsModule } from '@angular/forms';
 import { TextareaModule } from 'primeng/textarea';
 import { BaseLabelComponent } from "../../base-components/base-label/base-label.component";
 import { SignaturePadComponent } from "../../form/signature-pad/signature-pad.component";
 import { TranslatePipe } from 'src/app/shared/pipes';
+import { ProfileStore } from 'src/app/shared/stores/profile/profile.store';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-approve-reject-dialog',
@@ -33,6 +35,35 @@ export class ApproveRejectDialogComponent {
 
   existingSignature = model<string | null>(null);
   signature = model<string | null>(null);
+
+  private readonly profileStore = inject(ProfileStore);
+  private hasFetchedSignatureForCurrentOpen = false;
+
+  constructor() {
+    effect(() => {
+      const isVisible = this.visible();
+      const showPad = this.showSignaturePad();
+
+      if (isVisible && showPad) {
+        if (!this.hasFetchedSignatureForCurrentOpen) {
+          this.hasFetchedSignatureForCurrentOpen = true;
+          this.profileStore.getUserProfile()
+            .pipe(take(1))
+            .subscribe({
+              next: (res) => {
+                if (res.success && res.body?.signature) {
+                  this.existingSignature.set(res.body.signature);
+                  this.signature.set(res.body.signature);
+                }
+              },
+            });
+        }
+      } else if (!isVisible) {
+        this.hasFetchedSignatureForCurrentOpen = false;
+        this.existingSignature.set(null);
+      }
+    });
+  }
 
   isConfirmDisabled = computed(() => {
     const noteRequired = this.isRequired() && !this.note().trim();
