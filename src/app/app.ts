f695b5cc19b8c6d6c1bda9f04fa90a261a9d9600
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-03-16 17:18:37 +0200

    feat: Integrate PrimeNG date format handling and clean up date format attributes across various components for improved localization support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, signal, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, inject, OnInit, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { I18nService } from './shared/services/i18n/i18n.service';
 import { NgClass } from '@angular/common';
 import { DelegationCanceledDialogComponent } from './shared/components/utility-components/delegation-canceled-dialog/delegation-canceled-dialog.component';
 import { PrimengLocaleSyncService } from './core/services/primeng-locale-sync.service';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,14 @@ import { PrimengLocaleSyncService } from './core/services/primeng-locale-sync.se
 export class App implements OnInit {
   private readonly i18nService = inject(I18nService);
   protected readonly title = signal('benaa');
-
+  private primeNGConfig = inject(PrimeNG);
   constructor() {
     inject(PrimengLocaleSyncService); // Activate PrimeNG locale sync with app language
+    effect(() => {
+      this.primeNGConfig.setTranslation({
+        dateFormat: this.i18nService.currentLanguage() === 'ar' ? 'dd MM yy' : 'dd M yy',
+      })
+    })
   }
 
   ngOnInit(): void {
