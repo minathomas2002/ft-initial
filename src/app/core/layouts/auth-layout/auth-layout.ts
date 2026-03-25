a891acbe519bebd2ff854f25c0f97bf0f04d7561
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-03-25 19:56:08 +0200

    feat: Implement language switcher component and integrate it into the auth layout for improved localization

--- src/app/core/layouts/auth-layout/auth-layout.ts
+++ src/app/core/layouts/auth-layout/auth-layout.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthSideLayout } from './components/auth-side-layout/auth-side-layout';
 import { ImageErrorDirective } from 'src/app/shared/directives/image-error.directive';
+import { LanguageSwitcherComponent } from "src/app/shared/components/language-switcher";
+import { TranslatePipe } from 'src/app/shared/pipes';
 
 @Component({
   selector: 'app-auth-layout',
@@ -9,6 +11,8 @@ import { ImageErrorDirective } from 'src/app/shared/directives/image-error.direc
     RouterOutlet,
     AuthSideLayout,
     ImageErrorDirective,
+    LanguageSwitcherComponent,
+    TranslatePipe
   ],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
