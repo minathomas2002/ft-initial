f0f574e5ef4ddd418adc1b64fd0cda9cdfc41a3f
amr gamal <amr.ali@flairstech.com>
2026-03-29 11:41:47 +0200

    feat: Add language switcher component to visitor layout navbar

--- src/app/core/layouts/visitor-layout/components/visitor-layout-navbar/visitor-layout-navbar.ts
+++ src/app/core/layouts/visitor-layout/components/visitor-layout-navbar/visitor-layout-navbar.ts
@@ -3,10 +3,11 @@ import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { ImageErrorDirective } from 'src/app/shared/directives/image-error.directive';
 import { TranslatePipe } from 'src/app/shared/pipes';
+import { LanguageSwitcherComponent } from "src/app/shared/components/language-switcher";
 
 @Component({
   selector: 'app-visitor-layout-navbar',
-  imports: [ButtonModule, RouterLink, ImageErrorDirective, TranslatePipe],
+  imports: [ButtonModule, RouterLink, ImageErrorDirective, TranslatePipe, LanguageSwitcherComponent],
   templateUrl: './visitor-layout-navbar.html',
   styleUrl: './visitor-layout-navbar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
