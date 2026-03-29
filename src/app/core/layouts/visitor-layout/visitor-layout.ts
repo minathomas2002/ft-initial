d3e4684a0ddd50c2ab2e11d57b70933aa16c4611
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-03-29 11:35:50 +0200

    refactor: Update auth layout styles and replace background image for improved visual consistency

--- src/app/core/layouts/visitor-layout/visitor-layout.ts
+++ src/app/core/layouts/visitor-layout/visitor-layout.ts
@@ -1,8 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { VisitorLayoutNavbar } from "./components/visitor-layout-navbar/visitor-layout-navbar";
 import { RouterOutlet } from '@angular/router';
-import { LayoutFooterComponent } from '../../../shared/components/layout-components/layout-footer/layout-footer.component';
-
+import { LayoutFooterComponent } from "../main-layout/components/layout-footer/layout-footer.component";
 @Component({
   selector: 'app-visitor-layout',
   imports: [VisitorLayoutNavbar, RouterOutlet, LayoutFooterComponent],
