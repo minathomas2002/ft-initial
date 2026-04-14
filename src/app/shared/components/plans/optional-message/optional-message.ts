f7deb5d35e4fc2b58168e43096e311558430435e
mahmoud.salama153 <mahmoud.salama@flairstech.com>
2026-04-14 14:36:06 +0200

    refactor: Remove unused BaseTagComponent import from optional-message component

--- src/app/shared/components/plans/optional-message/optional-message.ts
+++ src/app/shared/components/plans/optional-message/optional-message.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BaseTagComponent } from '../../base-components/base-tag/base-tag.component';
 import { TranslatePipe } from 'src/app/shared/pipes';
 
 @Component({
   selector: 'app-optional-message',
-  imports: [BaseTagComponent, TranslatePipe],
+  imports: [TranslatePipe],
   templateUrl: './optional-message.html',
   styleUrl: './optional-message.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
